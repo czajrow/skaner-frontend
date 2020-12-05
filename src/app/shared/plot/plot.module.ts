70eab235df8180c323345a985e5d1334f8115731
Adam Czajka <czajka.adam147@gmail.com>
2020-12-05 21:49:48 +0100

    Create mocked plot

--- src/app/shared/plot/plot.module.ts
+++ src/app/shared/plot/plot.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlotComponent } from './plot.component';
+import * as PlotlyJS from 'plotly.js/dist/plotly.js';
+import { PlotlyModule } from 'angular-plotly.js';
 
-
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [PlotComponent],
   exports: [
-    PlotComponent
+    PlotComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    PlotlyModule,
   ]
 })
 export class PlotModule { }
