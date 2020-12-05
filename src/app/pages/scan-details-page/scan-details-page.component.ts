cc8281656150bfc3fa4ad16e12a76d3ed5f55d21
Adam Czajka <czajka.adam147@gmail.com>
2020-12-05 19:35:11 +0100

    Minor fix

--- src/app/pages/scan-details-page/scan-details-page.component.ts
+++ src/app/pages/scan-details-page/scan-details-page.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-scan-details-page',
   templateUrl: './scan-details-page.component.html',
   styleUrls: ['./scan-details-page.component.scss']
 })
-export class ScanDetailsPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ScanDetailsPageComponent {
 }
